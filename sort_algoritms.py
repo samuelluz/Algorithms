b508a0a2c9841c71b7d2bbf5f4260b9373a92ecd
Samuel Luz Gomes <samuelluz.g@gmail.com>
2019-09-28 15:46:38 -0300

    pythonic

--- sort_algoritms.py
+++ sort_algoritms.py
@@ -1,14 +1,13 @@
+from matplotlib import pyplot as plt
 def  selectionSort(array):
 
-    def swap(array, fristIndex, secondIndex):
-        temp = array[fristIndex]
-        array[fristIndex] = array[secondIndex]
-        array[secondIndex] = temp
+    def swap(array, first_index, second_index):
+        array[first_index], array[second_index] = array[second_index], array[first_index]
 
     def indexOfMinimum(array, startIndex):
         minIndex = startIndex
-        for i in range(startIndex, len(array)):
-            if array[i]<array[minIndex]:
+        for i, num in enumerate(array[startIndex:], startIndex):
+            if num<array[minIndex]:
                 minIndex = i
         return minIndex
 
@@ -17,5 +16,11 @@ def  selectionSort(array):
         swap(array, i, index)
 
 array = [22, 11, 99, 88, 9, 7, 42]
+print(array)
+plt.bar(range(len(array)),array)
+plt.show()
 selectionSort(array)
+plt.bar(range(len(array)),array)
+plt.show()
+print(array)
 assert array == [7, 9, 11, 22, 42, 88, 99]
